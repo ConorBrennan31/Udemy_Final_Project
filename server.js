9ab7239eb82499b4ac12480bcc15970a9f0b6673
Conor Brennan <conorbrennan@Conors-MacBook-Pro.local>
2020-06-18 15:04:51 -0400

    removing hardcoded users and changing / response

--- server.js
+++ server.js
@@ -23,37 +23,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			password: 'bananas',
-			email: 'sally@gmail.com',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-	{
-		id: '987',
-		hash: '',
-		email: 'john@gmail.com'
-	}
-
-	]
-}
-
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.send('it is working!');
 });
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })

b9312caab07b655ae3710ca2504fd1adb847493e
Conor Brennan <conorbrennan@Conors-MacBook-Pro.local>
2020-06-18 10:03:09 -0400

    committing all files

---
+++ controllers/image.js
@@ -0,0 +1,27 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'b655e776497f4c518f84dd622472c26a'
+});
+
+const handleApiCall = (req, res) => {
+	app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const id = req.body.id;
+	let found = false;
+	db('users').where('id', '=', id).increment('entries', 1).returning('entries').then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+}
